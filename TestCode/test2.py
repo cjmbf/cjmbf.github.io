f164b267239450ec1c07e5d006f19e1635331427
cjmbf <792233614@qq.com>
2021-05-19 08:53:54 +0700

    实验二

--- TestCode/test2.py
+++ TestCode/test2.py
@@ -14,10 +14,10 @@ plt.title(r'$f(x)=sin(x)$') #显示图形标题 f(x)=sin(x)
 plt.plot(x, y) #plt.plot()函数用于画图 它可以绘制点和线，以x为横轴，以y为纵轴绘制图像
 #plt.show()
 
-x1 = [t*0.375*np.pi for t in x]
-y1 = np.sin(x1)
+x1 = [t*0.375*np.pi for t in x] #定义y1中的参数 记3πt/8
+y1 = np.sin(x1)#定义信号y=sin(3πt/8)
 plt.subplot(1,2,2) #绘制多图，指一行有两列两个子图，这一图显示在第二列
 # plt.title(u"测试2") #注意：在前面加一个u
 plt.title(r'$f(x)=sin(\omega x), \omega = \frac{3}{8} \pi$') #显示图形标题f(x)=sin(wx),w=3π/8
-plt.plot(x1, y1)#plt.plot()函数用于画图 它可以绘制点和线，以x1为横轴，以y1为纵轴绘制图像
+plt.plot(x, y1)#plt.plot()函数用于画图 它可以绘制点和线，以x为横轴，以y1为纵轴绘制图像
 plt.show() #将图像显示
